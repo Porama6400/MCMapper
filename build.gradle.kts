108ff9f79945313852c58161f5865ca17104a19c
Porama6400 <porama6400@otlg.net>
2020-05-27 01:46:58 +0700

    Github Action & move bitumen into it's own project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "5.2.0"
+    maven
+    `maven-publish`
 }
 
 group = "net.otlg"
@@ -8,6 +10,7 @@ version = "1.3"
 
 repositories {
     mavenCentral()
+    maven("https://files.otlg.net/maven-repo/");
 }
 
 
@@ -17,14 +20,14 @@ configurations.implementation.get().extendsFrom(export);
 dependencies {
     testImplementation("junit", "junit", "4.12")
 
+    export("net.otlg:bitumen:1.0+")
+
     export("commons-cli:commons-cli:1.4");
-    export("org.jetbrains:annotations:19.0.0");
     export("commons-io:commons-io:2.6");
 
     export("org.ow2.asm:asm:8.0.1");
     export("org.ow2.asm:asm-commons:8.0.1");
     export("org.ow2.asm:asm-util:8.0.1");
-
 }
 
 configure<JavaPluginConvention> {
@@ -35,5 +38,23 @@ tasks.shadowJar {
     this.configurations.add(export);
     manifest {
         attributes["Main-Class"] = "net.otlg.mcmapper.MCMapper"
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GithubPackage"
+            url = uri("https://maven.pkg.github.com/Porama6400/MCMapper")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("GithubPackage") {
+            from(components["java"])
+        }
     }
 }
--- src/main/java/net/otlg/bitumen/pipe/Pipe.java
+++
@@ -1,8 +0,0 @@
-package net.otlg.bitumen.pipe;
-
-import net.otlg.bitumen.wrapper.Input;
-import net.otlg.bitumen.wrapper.Output;
-
-public interface Pipe {
-    void process(Input in, Output out);
-}
